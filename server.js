2e1621edfa4e9a6ea4df26963407722f60337003
Owais Master <owais.master@uconn.edu>
2020-01-13 20:07:23 -0500

    Initial Commit

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
+app.use(express.static("./client/public"))
 // Define API routes here
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 app.listen(PORT, () => {
